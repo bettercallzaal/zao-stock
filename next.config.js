225f1a82f207d2637204a1a3cc938f2a97ef2f2e
bettercallzaal <zaalp99@gmail.com>
2026-02-18 06:58:08 -0500

    fix: remove conflicting next.config.js - use next.config.cjs only

--- next.config.js
+++ next.config.js
@@ -1,1 +1,2 @@
-/** @type {import('next').NextConfig} */\nconst nextConfig = {\n  reactStrictMode: true,\n};\n\nexport default nextConfig;
+// This file is not used. All Next.js configuration is in next.config.cjs
+// Keeping this empty to prevent accidental loading by Next.js
